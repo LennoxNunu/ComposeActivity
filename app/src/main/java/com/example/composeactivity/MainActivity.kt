72aae9a925925882d1cdb2ff232b1d2b8a047232
LennoxNunu <lennoxnunu@gmail.com>
2024-01-27 17:59:07 +0200

    snackbar

--- app/src/main/java/com/example/composeactivity/MainActivity.kt
+++ app/src/main/java/com/example/composeactivity/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.composeactivity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.composeactivity.ui.theme.ComposeActivityTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +24,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    DisplaySnackBar()
                 }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter", "SuspiciousIndentation")
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun DisplaySnackBar() {
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+    val coroutineScope: CoroutineScope = rememberCoroutineScope()
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeActivityTheme {
-        Greeting("Android")
+    Scaffold(scaffoldState = scaffoldState) {
+        Button(onClick = {
+            coroutineScope.launch {
+              val snackBarResult =  scaffoldState.snackbarHostState.showSnackbar(
+                    message = "This is a message",
+                    actionLabel = "Undo",
+                    duration = SnackbarDuration.Long
+                )
+
+                when(snackBarResult){
+                    SnackbarResult.ActionPerformed -> Log.i("MyTag","action label clicked")
+                    SnackbarResult.Dismissed -> Log.i("MyTag","Dismissed!")
+                }
+            }
+        }) {
+            Text(text = "Display a SnackBar")
+        }
     }
+
 }
