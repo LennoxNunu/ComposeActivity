cbf670ea3d956b8b492572453ab19bf39e711338
LennoxNunu <lennoxnunu@gmail.com>
2024-01-28 09:53:27 +0200

    lazy column

--- app/src/main/java/com/example/composeactivity/MainActivity.kt
+++ app/src/main/java/com/example/composeactivity/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeactivity.ui.theme.ComposeActivityTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MyLazyColumn()
                 }
             }
         }
@@ -30,14 +35,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeActivityTheme {
-        Greeting("Android")
+fun MyLazyColumn(){
+    LazyColumn{
+      items(count = 100){
+          Text(
+              text = "User Name $it",
+              style = MaterialTheme.typography.h3,
+              modifier = Modifier.padding(10.dp)
+          )
+          Divider(color = Color.Black, thickness = 5.dp)
+      }
     }
 }
