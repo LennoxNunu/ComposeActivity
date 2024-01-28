6747df5fc0d8c1ba9378c5fbd0042971b20bb646
LennoxNunu <lennoxnunu@gmail.com>
2024-01-28 16:19:55 +0200

    minor project with lazy column

--- app/src/main/java/com/example/composeactivity/MainActivity.kt
+++ app/src/main/java/com/example/composeactivity/MainActivity.kt
@@ -4,19 +4,17 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composeactivity.compose.TvShowListItem
+import com.example.composeactivity.data.TvShowList
+import com.example.composeactivity.model.TvShow
 import com.example.composeactivity.ui.theme.ComposeActivityTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,8 +27,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MyLazyColumn{
-                        Toast.makeText(this,it,Toast.LENGTH_LONG).show()
+                    DisplayTvShows{
+                        startActivity(InfoActivity.intent(this,it))
                     }
                 }
             }
@@ -38,17 +36,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun MyLazyColumn(selectedItem: (String)->(Unit)){
-    LazyColumn{
-      items(count = 100){
-          Text(
-              text = "User Name $it",
-              style = MaterialTheme.typography.h3,
-              modifier = Modifier.padding(10.dp)
-                  .clickable { selectedItem("$it selected") }
-          )
-          Divider(color = Color.Black, thickness = 5.dp)
-      }
+fun DisplayTvShows(selectedItem: (TvShow) -> (Unit)){
+    val tvShows = remember{TvShowList.tvShows}
+    LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)){
+        items(items = tvShows , itemContent = {
+            TvShowListItem(tvShow = it, selectedItem = selectedItem)
+        })
     }
 }
+
+
