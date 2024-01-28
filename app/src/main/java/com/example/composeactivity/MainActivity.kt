a72cf510091413a3efc990603f02e53aad84fb89
LennoxNunu <lennoxnunu@gmail.com>
2024-01-28 09:35:40 +0200

    scrollable column

--- app/src/main/java/com/example/composeactivity/MainActivity.kt
+++ app/src/main/java/com/example/composeactivity/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.composeactivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeactivity.ui.theme.ComposeActivityTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +30,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ScrollableColumn()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeActivityTheme {
-        Greeting("Android")
+fun ScrollableColumn(){
+    var scrollState = rememberScrollState()
+    Column(modifier = Modifier.verticalScroll(state = scrollState)) {
+        for (i in 1..100){
+            Text(
+                text = "User Name $i",
+                style = MaterialTheme.typography.h3,
+                modifier = Modifier.padding(10.dp)
+                )
+            Divider(color = Color.Black, thickness = 5.dp)
+        }
     }
 }
