bac494ed5400c75d192421359f9e40c2886c5723
LennoxNunu <lennoxnunu@gmail.com>
2024-01-29 14:13:38 +0200

    unidirectional data flow and state hoisting

--- app/src/main/java/com/example/composeactivity/MainActivity.kt
+++ app/src/main/java/com/example/composeactivity/MainActivity.kt
@@ -26,7 +26,8 @@ class MainActivity : ComponentActivity() {
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    MyButton()
+                    var count by remember{mutableStateOf(0)}
+                    MyButton(currentCount = count,updateCount = {count = it + 1})
                 }
             }
         }
@@ -35,14 +36,14 @@ class MainActivity : ComponentActivity() {
 
 
 
-@Preview(name = "MyButton")
+
 @Composable
-fun MyButton(){
-    val context = LocalContext.current
-    var count by remember{mutableStateOf(0)}
+fun MyButton(currentCount:Int,updateCount:(Int)->Unit){
+
+
     Button(onClick = {
-        count += 1
-        Toast.makeText(context, "Count is : $count", Toast.LENGTH_SHORT).show()
+       updateCount(currentCount)
+
     },
         contentPadding = PaddingValues(16.dp),
         border = BorderStroke(10.dp, Color.Black),
@@ -51,7 +52,7 @@ fun MyButton(){
             contentColor = Color.White
         )
     ) {
-        Text("Count is : $count",
+        Text("Count is : $currentCount",
             style = MaterialTheme.typography.h3,
             modifier = Modifier.padding(5.dp)
         )
