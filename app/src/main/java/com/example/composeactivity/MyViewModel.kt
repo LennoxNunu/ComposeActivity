edcad73fca5bd8ba054e415c1aaf9517ae0c9e46
LennoxNunu <lennoxnunu@gmail.com>
2024-01-30 17:49:27 +0200

    buffer collect collectLatest

--- app/src/main/java/com/example/composeactivity/MyViewModel.kt
+++ app/src/main/java/com/example/composeactivity/MyViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.composeactivity
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MyViewModel: ViewModel() {
     val myFlow = flow<Int>{
@@ -11,4 +16,28 @@ class MyViewModel: ViewModel() {
             delay(1000L)
         }
     }
+
+    init {
+        backPressureDemo()
+    }
+
+    private fun backPressureDemo(){
+        val myFlow1 = flow<Int> {
+            for (i in 1..10){
+                Log.i("MYTAG","Produced $i")
+                emit(i)
+                delay(1000L)
+            }
+        }
+
+        viewModelScope.launch {
+            // myFlow1.collect {
+            // myFlow1.buffer().collect {
+             myFlow1.collectLatest {
+                delay(2000L)
+                Log.i("MYTAG","Consumed $it")
+            }
+        }
+    }
+
 }
