21dda263e1921b91817de962243dde364a80aa14
LennoxNunu <lennoxnunu@gmail.com>
2024-01-30 16:58:22 +0200

    kotlin flows with view model

--- app/src/main/java/com/example/composeactivity/MainActivity.kt
+++ app/src/main/java/com/example/composeactivity/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.composeactivity.ui.theme.ComposeActivityTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
@@ -20,14 +21,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val myFlow = flow<Int>{
-            for (i in 1..100){
-                emit(i)
-                delay(1000L)
-            }
-        }
+
         setContent {
-            val currentValue = myFlow.collectAsState(initial = 1).value
+            val viewModel = viewModel<MyViewModel>()
+            val currentValue = viewModel.myFlow.collectAsState(initial = 1).value
             ComposeActivityTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
